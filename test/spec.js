4a11ae69e1a16ce450a046214bd8958196342a69
Simon Willcock <simon@willcock.com.au>
2015-11-21 18:16:36 +1100

    More changes to test testing framework and travis CI

--- test/spec.js
+++ test/spec.js
@@ -7,27 +7,28 @@
 'use strict'
 
 var assert = require('assert'),
-lib        = require('../lib/gitlabjs'),
-$ = jQuery = require('../node_modules/jquery/dist/jquery');
+GitLabClient        = require('../lib/GitLabClient'),
+jQuery = require('../node_modules/jquery/dist/jquery');
 
 describe('gitlabjs init test', function() {
-    // var url = 'http://10.61.4.51:8011',
-    //     token = 'abc123',
-    //     $;
+    var url = 'http://10.61.4.51:8011',
+        token = 'abc123',
+        $;
     // beforeEach(function () {
     //     $ = jQuery = require('jquery');
     // });
 
-    // var client = lib.gitlabjs(url,token);
-    // it('should return the token passed during initialisation', function() {
-    //     assert.equal(client.token,token);    
-    // });
+    var client = GitLabClient({
+        'api': url,
+        'token': token,
+        'oAuth': true
+    });
+    it('should return the token passed during initialisation', function() {
+        assert.equal(client.token,token);
+    });
 
-    // it('should return the url passed during initialisation', function() {    
+    // it('should return the url passed during initialisation', function() {
     //     assert.equal(client.url,url);
     // });
 
-    // it('should not expose private methods', function() {    
-    //     assert.equal(typeof client._private,'undefined');
-    // })
 });
